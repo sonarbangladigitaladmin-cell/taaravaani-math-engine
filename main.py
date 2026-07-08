f358dba335b179f730022b091ac952f1a33f106c
sonarbangladigitaladmin-cell <sonarbangladigitaladmin@gmail.com>
2026-07-08 23:57:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origin_regex=r"^https://(www\.)?sonarbangladigital\.com$|^https://expert\.sonarbangladigital\.com$|^https://admin\.sonarbangladigital\.com$|^http://localhost(:\d+)?$",
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
